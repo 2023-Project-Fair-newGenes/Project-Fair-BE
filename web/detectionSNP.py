4732443ba1de4b82663030cb696e22db7c2574d6
Yujin-nKim <yujinkimvv@gmail.com>
2023-05-27 20:55:41 +0900

    디버깅 메세지 추가

--- web/detectionSNP.py
+++ web/detectionSNP.py
@@ -8,17 +8,32 @@ def detection(snp_filepath):
     id_list = GenomeCharacteristics.objects.values_list('snp_id', flat=True)
     id_list = list(id_list)
 
+    print("-------------------------------------------------------------------")
+    print("다음의 비만 유전자 snp를 보유하고 있는지 확인합니다...")
+    print(id_list)
+
     user_snp = pd.read_csv(snp_filepath)
 
     #사용자 genome SNP file에서 타게팅 비만 유전자 SNP rsid가 있는지 확인
     find_snp_list = []
+    no_snp_list = []
     for rsid in id_list:
         find_snp = user_snp.loc[user_snp['# rsid'] == rsid]
         if find_snp.empty != True:
             find_snp_list.append(find_snp['# rsid'].values[0])
+        else:
+            no_snp_list.append(rsid)
+
+    print("-------------------------------------------------------------------")
+    print("사용자의 보유 SNP list")
+    print(find_snp_list)
+    print("사용자의 미보유 SNP list")
+    print(no_snp_list)
 
     if os.path.isfile(snp_filepath):
         os.remove(snp_filepath)
+        if os.path.isfile(snp_filepath) != True:
+            print("파일이 삭제 되었습니다.")
     else:
         print("지우고자 하는 파일 없음")
 
@@ -29,10 +44,19 @@ def getCharacteristic(find_snp_list):
     for _rsid in find_snp_list:
         dict = {}
         info = GenomeCharacteristics.objects.get(snp_id=_rsid)
+
         dict['id'] = _rsid
         dict['snp_name'] = info.snp_name
         dict['characteristics'] = info.characteristics
         result.append(dict)
+
+    print("-------------------------------------------------------------------")
+    print("다음 snp에 대한 정보가 전달됩니다.")
+    info_list = []
+    for item in result:
+        info_list.append(item['id'])
+
+    print(info_list)
 
     return result
 
--- web/views.py
+++ web/views.py
@@ -19,9 +19,7 @@ class Test(APIView):
 class Result(APIView):
     def get(self, request):
 
-        # user_id = request.data.get('user_id')
         user_id = request.query_params.get('user_id')
-        print(user_id)
 
         csv_filename = user_id + ".csv"
         directory_name = 'genome_file'
@@ -29,12 +27,10 @@ class Result(APIView):
         current_directory = os.getcwd()
         csv_path = os.path.join(current_directory, directory_name)
         csv_path = os.path.join(csv_path, csv_filename)
-        print(csv_path)
 
         if os.path.isfile(csv_path):
             find_snp_list = detection(csv_path)
             result = getCharacteristic(find_snp_list)
-            print(result)
         else:
             error_message = "CSV 파일 " + csv_filename + "가 존재하지 않습니다."
             return HttpResponseNotFound(error_message)
@@ -44,16 +40,13 @@ class Result(APIView):
 class Upload(APIView):
     def post(self, request):
         user_id = request.data.get('user_id') #사용자 아이디 6자리
-        print("user_id | ", user_id)
         genome_file = request.FILES.get('genome_file') #게놈 데이터 파일
-        print("geome_file | ", genome_file)
-        print("request | " , request)
-        print("request.data | ", request.data)
-        print("request.FILES | ", request.FILES)
+
         file_storage = FileSystemStorage()
         filename = file_storage.save("genome_file/" + user_id + ".csv", genome_file)
 
         if filename:
+            print("사용자의 게놈 데이터가 저장되었습니다.")
             return Response(status=status.HTTP_200_OK)
         else:
             return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
@@ -72,7 +65,5 @@ class RequestInfo(APIView):
         result = {}
         result['id'] = rsid
         result['lifestyle_info'] = lifestyle_info
-
-        print(result)
 
         return Response(result)
